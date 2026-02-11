44302c398e5da75e54ccc34e39bf05b2f061da9a
Nishanth Narayana K V <mail4kvnishanth@gmail.com>
2026-02-11 14:23:39 +0530

    Create Arr.java

---
+++ JavaTest/Arr.java
@@ -0,0 +1,11 @@
+
+public class Arr{
+	public static void main(String args[])
+	{
+		ArrayList<Integer> arr= new ArrayList<Integer>();
+		arr.add(123);	
+		arr.add(12);
+		arr.add(4343);
+		System.out.print(arr);
+	}
+}
