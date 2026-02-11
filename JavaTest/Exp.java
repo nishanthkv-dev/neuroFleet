2fbfb02ba34ef6fa7c5f5944cbb0bc9a345b6aec
Nishanth Narayana K V <mail4kvnishanth@gmail.com>
2026-02-11 14:22:24 +0530

    Update Exp.java

--- JavaTest/Exp.java
+++ JavaTest/Exp.java
@@ -1,6 +1,10 @@
 public class Exp {
   public static void main(String[ ] args) {
-    int[] myNumbers = {1, 2, 3};
-    System.out.println(myNumbers[10]); // error!
+    try {
+      int[] myNumbers = {1, 2, 3};
+      System.out.println(myNumbers[10]);
+    } catch (Exception e) {
+      System.out.println("Something went wrong.");
+    }
   }
 }
